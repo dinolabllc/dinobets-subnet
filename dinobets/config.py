343935fee82d4fef66e8961af82d7711539300f3
captain <techalpinist@gmail.com>
2025-12-28 23:21:46 -0300

    fix config

--- dinobets/config.py
+++ dinobets/config.py
@@ -4,6 +4,5 @@ import os
 load_dotenv()
 
 NETWORK = os.getenv('NETWORK', 'test')
-# API_ROOT = 'https://bittensor.dinobets.io' if NETWORK == 'finney' else 'https://stagingbittensor.dinobets.io'
-API_ROOT = 'http://localhost:5001'
-SEVER_SEED = os.getenv('SERVER_SEED', '4F8D2EF3C9E109F9E8A8B52B872')
+API_ROOT = 'https://bittensor.dinobets.io' if NETWORK == 'finney' else 'https://stagingbittensor.dinobets.io'
+SEVER_SEED = os.getenv('SERVER_SEED', '19c7cd7a575130d3d7c93bbbd3e50296ac7d4b8fc608347009bde24eef19b798')
